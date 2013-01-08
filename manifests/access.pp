bf0e359dbdc22258475533b6b9dbad27d48ba5b8
Keith Burdis <keith@burdis.org>
2013-01-08 16:40:39 +0000

    Check that arrays aren't empty before creating resources.  Fix silly typos.

--- manifests/access.pp
+++ manifests/access.pp
@@ -3,24 +3,32 @@ class ssh::access (
   $deny  = [],
 ) {
   include augeasproviders
-  $allow_groups = grep($allow, '/^@')
-  $deny_groups  = grep($deny,  '/^@')
-  $allow_users  = grep($allow, '/^[^@]')
-  $deny_users   = grep($deny,  '/^[^@]')
-  create_resources(
-    sshd_config,
-    { 'AllowGroups' => { 'value' => $allow_groups, '/^@') } }
-  )
-  create_resources(
-    sshd_config,
-    { 'DenyGroups'  => { 'value' => $deny_groups, '/^@') } }
-  )
-  create_resources(
-    sshd_config,
-    { 'AllowUsers'  => { 'value' => $allow_users, '/^[^@]') } }
-  )
-  create_resources(
-    sshd_config,
-    { 'DenyUsers'   => { 'value' => $deny_users, '/^[^@]') } }
-  )
+  $allow_groups = grep($allow, '^@')
+  $deny_groups  = grep($deny,  '^@')
+  $allow_users  = grep($allow, '^[^@]')
+  $deny_users   = grep($deny,  '^[^@]')
+  if size($allow_groups) > 0 {
+    create_resources(
+      sshd_config,
+      { 'AllowGroups' => { 'value' => $allow_groups, } }
+    )
+  }
+  if size($deny_groups) > 0 {
+    create_resources(
+      sshd_config,
+      { 'DenyGroups'  => { 'value' => $deny_groups, } }
+    )
+  }
+  if size($allow_users) > 0 {
+    create_resources(
+      sshd_config,
+      { 'AllowUsers'  => { 'value' => $allow_users, } }
+    )
+  }
+  if size($allow_groups) > 0 {
+    create_resources(
+      sshd_config,
+      { 'DenyUsers'   => { 'value' => $allow_groups, } }
+    )
+  }
 }
